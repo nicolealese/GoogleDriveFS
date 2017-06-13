8ce6a1494b7f55c527e1f64855359cf1533292cd
Nicole Andrews <andre22n@mtholyoke.edu>
2017-06-13 10:45:58 -0400

    changed file names to path names

--- html/client.js
+++ html/client.js
@@ -38,196 +38,201 @@ function handleAuthResult(authResult) {
 }
 
 function makeDirectory() {
-  var pathName = prompt("Enter the path name : ", "path name here");
-  if (pathName === null) {
-    console.log("no valid path name");
-  }
-  else {
-  mkdir(pathName);
-  }
+    var pathName = prompt("Enter the path name : ", "path name here");
+    if (pathName === null) {
+        console.log("no valid path name");
+    } else {
+        mkdir(pathName);
+    }
 }
 
 function mkdir(p) {
-const title = path.basename(p);
-const dir = path.dirname(p);
-const base = path.basename(dir);
+    const title = path.basename(p);
+    const dir = path.dirname(p);
+    const base = path.basename(dir);
 
-   var request = gapi.client.drive.files.list({
-     "q": "title = '" + base + "'"
-   });
-   request.execute(function(resp) {
-      var id = resp.items[0].id;
-      console.log('id in callback = ' + id)
+    var request = gapi.client.drive.files.list({
+        "q": "title = '" + base + "'"
+    });
+    request.execute(function(resp) {
+        var id = resp.items[0].id;
+        console.log('id in callback = ' + id)
 
-      var access_token = oauthToken;
-      var secondRequest = gapi.client.request({
-          'path': '/drive/v2/files/',
-          'method': 'POST',
-          'headers': {
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer ' + access_token,
-          },
-          'body':{
-              "title" : title,
-              "parents": [
-                {
-                "id": id
-              }
-              ],
-              "mimeType" : "application/vnd.google-apps.folder",
-          }
-      });
+        var access_token = oauthToken;
+        var secondRequest = gapi.client.request({
+            'path': '/drive/v2/files/',
+            'method': 'POST',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access_token,
+            },
+            'body': {
+                "title": title,
+                "parents": [{
+                    "id": id
+                }],
+                "mimeType": "application/vnd.google-apps.folder",
+            }
+        });
 
-secondRequest.execute(function (resp) {
-  console.log('nested folder done creating')
-})
-});
+        secondRequest.execute(function(resp) {
+            console.log('nested folder done creating')
+        })
+    });
 }
 
 function trashFile() {
-  var fileName = prompt("Enter the file name : ", "file name here");
-  if (fileName === null) {
-    console.log("no valid file name");
-  }
-  else {
-  trashOldFile(fileName);
-  }
+    var pathName = prompt("Enter the path name : ", "path name here");
+    if (pathName === null) {
+        console.log("no valid path name");
+    } else {
+        trashOldFile(pathName);
+    }
 }
 
 /**
  * Move a file to the trash.
  *
- * @param {String} fileName Name of the file to trash.
+ * @param {String} p Path of the file to trash.
  */
-function trashOldFile(fileName) {
-  var request = gapi.client.drive.files.list({
-    "q": "title = '" + fileName + "'"
-  });
-  request.execute(function(resp) {
-     var id = resp.items[0].id;
+function trashOldFile(p) {
+    const title = path.basename(p);
+    const dir = path.dirname(p);
+    const base = path.basename(dir);
 
-     var secondRequest = gapi.client.drive.files.trash({
-    'fileId': id
-  });
-  secondRequest.execute(function(resp) { });
-});
+    var request = gapi.client.drive.files.list({
+        "q": "title = '" + title + "'"
+    });
+    request.execute(function(resp) {
+        var id = resp.items[0].id;
+
+        var secondRequest = gapi.client.drive.files.trash({
+            'fileId': id
+        });
+        secondRequest.execute(function(resp) {});
+    });
 }
 
 function writeFile() {
-  var fileName = prompt("Enter the file name : ", "file name here");
-  if (fileName === null) {
-    console.log("no valid file name");
-  }
-  else {
-  writeAFile(fileName);
-  }
+    var pathName = prompt("Enter the path name : ", "path name here");
+    if (pathName === null) {
+        console.log("no valid path name");
+    } else {
+        writeAFile(pathName);
+    }
 }
 
-function writeAFile(fileName, callback) {
-  var request = gapi.client.drive.files.list({
-    "q": "title = '" + fileName + "'"
-  });
-  request.execute(function(resp) {
-     var id = resp.items[0].id;
-  var text = prompt("Enter your text here: ", "text here");
-  const boundary = '-------314159265358979323846';
-  const delimiter = "\r\n--" + boundary + "\r\n";
-  const close_delim = "\r\n--" + boundary + "--";
+function writeAFile(p, callback) {
+    const title = path.basename(p);
+    const dir = path.dirname(p);
+    const base = path.basename(dir);
 
-  var contentType = "text/html";
-  var metadata = {
-      'mimeType': contentType,
-  };
+    var request = gapi.client.drive.files.list({
+        "q": "title = '" + title + "'"
+    });
+    request.execute(function(resp) {
+        var id = resp.items[0].id;
+        var text = prompt("Enter your text here: ", "text here");
+        const boundary = '-------314159265358979323846';
+        const delimiter = "\r\n--" + boundary + "\r\n";
+        const close_delim = "\r\n--" + boundary + "--";
 
-  var multipartRequestBody =
-      delimiter + 'Content-Type: application/json\r\n\r\n' +
-      JSON.stringify(metadata) +
-      delimiter + 'Content-Type: ' + contentType + '\r\n' + '\r\n' +
-      text +
-      close_delim;
+        var contentType = "text/html";
+        var metadata = {
+            'mimeType': contentType,
+        };
 
-  if (!callback) {
-      callback = function(file) {
-          console.log("Update Complete ", file)
-      };
-  }
+        var multipartRequestBody =
+            delimiter + 'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter + 'Content-Type: ' + contentType + '\r\n' + '\r\n' +
+            text +
+            close_delim;
 
-  gapi.client.request({
-      'path': '/upload/drive/v3/files/' + id + "&uploadType=multipart",
-      'method': 'PATCH',
-      'params': {
-          'fileId': id,
-          'uploadType': 'multipart'
-      },
-      'headers': {
-          'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
-      },
-      'body': multipartRequestBody,
-      callback: callback,
-  });
-});
+        if (!callback) {
+            callback = function(file) {
+                console.log("Update Complete ", file)
+            };
+        }
+
+        gapi.client.request({
+            'path': '/upload/drive/v3/files/' + id + "&uploadType=multipart",
+            'method': 'PATCH',
+            'params': {
+                'fileId': id,
+                'uploadType': 'multipart'
+            },
+            'headers': {
+                'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+            },
+            'body': multipartRequestBody,
+            callback: callback,
+        });
+    });
 
 }
 
 function readFile() {
-  var fileName = prompt("Enter the file name : ", "file name here");
-  if (fileName === null) {
-    console.log("no valid file name");
-  }
-  else {
-  readAFile(fileName);
-  }
+    var pathName = prompt("Enter the path name : ", "path name here");
+    if (pathName === null) {
+        console.log("no valid path name");
+    } else {
+        readAFile(pathName);
+    }
 }
 
-function readAFile(fileName) {
-  var request = gapi.client.drive.files.list({
-    "q": "title = '" + fileName + "'"
-  });
-  request.execute(function(resp) {
-     var id = resp.items[0].id;
-                // make the request to the google drive server
-                gapi.client.request({
-                    path: '/drive/v2/files/' + id,
-                    method: 'GET',
-                    callback: function(obj) {
-                        const xhr = new XMLHttpRequest();
-                        xhr.open("GET", obj.downloadUrl);
-                        xhr.setRequestHeader("Authorization", "Bearer " + gapi.auth.getToken().access_token);
-                        xhr.onload = function() {
-                            console.log(xhr.response);
-                            var data = xhr.response;
-                            var output = document.getElementById("output");
-                            docID = id;
-                            document.write(data);
-                        }
-                        xhr.send();
-                    }
-                });
-              });
+function readAFile(p) {
+    const title = path.basename(p);
+    const dir = path.dirname(p);
+    const base = path.basename(dir);
+
+    var request = gapi.client.drive.files.list({
+        "q": "title = '" + title + "'"
+    });
+    request.execute(function(resp) {
+        var id = resp.items[0].id;
+        // make the request to the google drive server
+        gapi.client.request({
+            path: '/drive/v2/files/' + id,
+            method: 'GET',
+            callback: function(obj) {
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", obj.downloadUrl);
+                xhr.setRequestHeader("Authorization", "Bearer " + gapi.auth.getToken().access_token);
+                xhr.onload = function() {
+                    console.log(xhr.response);
+                    var data = xhr.response;
+                    var output = document.getElementById("output");
+                    docID = id;
+                    document.write(data);
+                }
+                xhr.send();
             }
+        });
+    });
+}
 
 
 
 function createNewFile() {
-  var pathName = prompt("Enter the path name : ", "path name here");
-  if (pathName === null) {
-    console.log("no valid path name");
-  }
-  else {
-  createFile(pathName);
-  }
+    var pathName = prompt("Enter the path name : ", "path name here");
+    if (pathName === null) {
+        console.log("no valid path name");
+    } else {
+        createFile(pathName);
+    }
 }
 
 
 function createFile(p) {
-  const title = path.basename(p);
-  const dir = path.dirname(p);
-  const base = path.basename(dir);
+    const title = path.basename(p);
+    const dir = path.dirname(p);
+    const base = path.basename(dir);
 
-     var request = gapi.client.drive.files.list({
-       "q": "title = '" + base + "'"
-     });
-     request.execute(function(resp) {
+    var request = gapi.client.drive.files.list({
+        "q": "title = '" + base + "'"
+    });
+    request.execute(function(resp) {
         var id = resp.items[0].id;
 
         var access_token = oauthToken;
@@ -238,30 +243,28 @@ function createFile(p) {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + access_token,
             },
-            'body':{
-                "title" : title,
-                "parents": [
-                  {
-                  "id": id
-                }
-                ],
-                "mimeType" : "text/html",
+            'body': {
+                "title": title,
+                "parents": [{
+                    "id": id
+                }],
+                "mimeType": "text/html",
             }
         });
 
-  secondRequest.execute(function (resp) {
-    console.log('nested file done creating')
-  })
-});
+        secondRequest.execute(function(resp) {
+            console.log('nested file done creating')
+        })
+    });
 }
 
 
 
 module.exports = {
-  onApiLoad,
-  makeDirectory,
-  createNewFile,
-  trashFile,
-  readFile,
-  writeFile,
+    onApiLoad,
+    makeDirectory,
+    createNewFile,
+    trashFile,
+    readFile,
+    writeFile,
 };
